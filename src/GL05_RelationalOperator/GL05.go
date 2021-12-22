3580e7c8727dfd59f2f3446151381605cbc8de17
Jerry Cheng <zychengjerry@gmail.com>
2021-12-22 16:17:57 +0800

    update GL04, GL05, GL06

---
+++ src/GL05_RelationalOperator/GL05.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 15
+	var b int = 7
+
+	if a == b {
+		fmt.Printf("第一行 - a 等于 b\n")
+	} else {
+		fmt.Printf("第一行 - a 不等于 b\n")
+	}
+	if a < b {
+		fmt.Printf("第二行 - a 小于 b\n")
+	} else {
+		fmt.Printf("第二行 - a 不小于 b\n")
+	}
+
+	if a > b {
+		fmt.Printf("第三行 - a 大于 b\n")
+	} else {
+		fmt.Printf("第三行 - a 不大于 b\n")
+	}
+
+	/* Lets change value of a and b */
+	a = 7
+	b = 15
+	if a <= b {
+		fmt.Printf("第四行 - a 小于等于 b\n")
+	}
+	if b >= a {
+		fmt.Printf("第五行 - b 大于等于 a\n")
+	}
+}
