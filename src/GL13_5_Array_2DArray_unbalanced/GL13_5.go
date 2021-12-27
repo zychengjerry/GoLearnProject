00dcfaaafd96d8902c49d65625bf45fd4e9dc5cb
Jerry Cheng <zychengjerry@gmail.com>
2021-12-27 18:57:20 +0800

    update GL13_4,5

---
+++ src/GL13_5_Array_2DArray_unbalanced/GL13_5.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 创建空的二维数组
+	animals := [][]string{}
+
+	// 创建三个一维数组，各数组长度不同
+	row1 := []string{"fish", "shark", "eel"}
+	row2 := []string{"bird"}
+	row3 := []string{"lizard", "salamander"}
+
+	// 使用 append() 函数将一维数组添加到二维数组中
+	animals = append(animals, row1)
+	animals = append(animals, row2)
+	animals = append(animals, row3)
+
+	// 循环输出
+	for i := range animals {
+		fmt.Printf("Row: %v\n", i)
+		fmt.Println(animals[i])
+	}
+}
+
+/*
+Row: 0
+[fish shark eel]
+Row: 1
+[bird]
+Row: 2
+[lizard salamander]
+*/
